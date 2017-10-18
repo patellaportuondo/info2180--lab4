7c6f1b92723123d9b37db88e0c1679876d6cfa52
patellaportuondo <32628686+patellaportuondo@users.noreply.github.com>
2017-10-18 11:28:00 -0500

    exercise2

--- maze.js
+++ maze.js
@@ -1,12 +1,23 @@
 function  setUpEvent(){
-	var Touch = document.getElementById("boundary1");
-
-	Touch.onmouseover =function(){
-		Touch.classList.add("youlose");
+	var touch = document.getElementById("boundary1");
+	var bound = document.querySelectorAll(".boundary");
+	
+	touch.onmouseover =function(){
+		touch.classList.add("youlose");
 	}
+	for (var i = 0; i < bound.length-1; i++) {
+		bound[i].onmouseover= function(){
+			for (var i = 0; i < bound.length-1; i++) {
+				bound[i].classList.add("youlose");
+			}
+		}
+
+	}
+
 
 }
 
 window.onload = function(){ 
 	setUpEvent();
+
 }
